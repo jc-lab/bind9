7ad0cdc56d796df8a2d0bb8ab09968a76f20d9a8
Jichan <development@jc-lab.net>
2020-03-15 21:57:02 +0900

    add initial files

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM alpine:latest
+
+MAINTAINER resyst-it <florian.cauzardjarry@gmail.com>
+
+RUN apk --update add bind && rm -rf /var/cache/apk/*
+
+COPY files/named.conf /etc/bind/named.conf
+
+EXPOSE 953/tcp 53/tcp 53/udp
+
+CMD ["named", "-c", "/etc/bind/named.conf", "-g", "-u", "named"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "x$RNDC_BIND_ADDRESS" == "x" ]; then
+	RNDC_BIND_ADDRESS="localhost"
+fi
+
+if [ "x$RNDC_ALGORITHM$RNDC_SECRET" != "x" ]; then
+	echo 'key "rndc-key" {' > /etc/bind/rndc.key
+	echo '     algorithm'$RNDC_ALGORITHM';' >> /etc/bind/rndc.key
+	echo '     secret "'$RNDC_SECRET'";' >> /etc/bind/rndc.key
+	echo '};' >> /etc/bind/rndc.key
+	
+	echo 'include "/etc/bind/rndc.key";' > /etc/bind/rndc.conf
+	echo 'options { default-server '$RNDC_BIND_ADDRESS'; default-key "rndc-key"; };' >> /etc/bind/rndc.conf
+fi
+
