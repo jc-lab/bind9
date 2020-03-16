f67909135dc16a24f5ee53fece6d379a6ec7b037
Jichan <development@jc-lab.net>
2020-03-16 11:48:29 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ fi
 [ ! -d /etc/bind/zones/ ] && mkdir -p /etc/bind/zones/
 
 for name in $(ls /etc/bind/zones/*.conf); do
-	echo "include \"$name\"" >> /etc/bind/named.conf
+	echo "include \"$name\";" >> /etc/bind/named.conf
 done
 
 /usr/sbin/named -c /etc/bind/named.conf -g -u named
